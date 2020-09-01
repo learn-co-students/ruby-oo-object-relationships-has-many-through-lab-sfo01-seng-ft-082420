7f05cd9458129e342e341c33655f0745d3f4f696
mehmehmehlol <hkmeganlo@gmail.com>
2020-08-31 23:31:22 -0700

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,30 @@
+class Doctor
+
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_appointment(patient, date)
+        Appointment.new(date, patient, self)
+    end
+
+    def appointments
+        Appointment.all.select{|app| app.doctor == self}
+    end
+
+    def patients
+        appointments.collect {|app| app.patient}
+    end
+
+
+end
